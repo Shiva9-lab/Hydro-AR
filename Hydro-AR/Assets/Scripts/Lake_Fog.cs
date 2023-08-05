f28eedbfc308ed98763797a7e7c240744b53e4eb
Tanish-Raorane <tanishraorane4@gmail.com>
2023-08-05 15:45:59 +0530

    Animation

---
+++ Hydro-AR/Assets/Scripts/Lake_Fog.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lake_Fog : MonoBehaviour
+{
+    private Animator animator;
+    int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(animator != null)
+        {
+            if(Input.GetKeyDown(KeyCode.Q))
+            {
+                count++;
+                if (count == 1)
+                {
+                    animator.SetTrigger("Tap_lake_Fog");
+                    animator.SetTrigger("Tap_Ocean_Fog");
+                }
+
+                if(count == 2)
+                {
+                    animator.SetTrigger("Cloud 1_Formation");
+                    animator.SetTrigger("Cloud 2_Formation");
+                    animator.SetTrigger("Cloud 3_Formation");
+                }
+
+                if(count == 3)
+                {
+                    animator.SetTrigger("Cloud 1_Movement");
+                    animator.SetTrigger("Cloud 2_Movement");
+                    animator.SetTrigger("Cloud 3_Movement");
+                }
+            }
+        }
+    }
+}
